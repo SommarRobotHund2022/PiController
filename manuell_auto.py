53fb8be829b860d598a6a2db365793776d5fa1ef
Robin <robinekroth95@hotmail.com>
2022-07-11 15:10:52 +0200

    add piserver

--- manuell_auto.py
+++ manuell_auto.py
@@ -1,10 +1,10 @@
 from enum import Enum
 import threading
-from OpenCatSerial import *
 import auto
 import manual
 import zmq
 PORT = "/dev/ttyS0"
+from piserver import Queue_MD, Queue_ST, t
 
 class Modes(Enum):
     MANUAL = 0
@@ -12,42 +12,22 @@ class Modes(Enum):
     DAY = 2
     NIGHT = 3
 
-context = zmq.Context()
-sub_sock = context.socket(zmq.SUB)
-sub_sock.connect("tcp://127.0.0.1:2273")
-sub_sock.setsockopt(zmq.SUBSCRIBE, b'MD:')
-sub_sock.setsockopt(zmq.SUBSCRIBE, b'ST:')
-
 MODES = [manual, auto, None, None]
 MODE = Modes.AUTO
 
 def bg():
     global MODE
     while True:
-        print("asdasdasasd")
-        cmd = sub_sock.recv().decode('utf-8').replace('MD:', '').strip().upper()
-        print(cmd)
-        if cmd == Modes.MANUAL.name:
-            print("Switch manual")
-            MODE = Modes.MANUAL
-        elif cmd == Modes.AUTO.name:
-            print("Switch Auto")
-            MODE = Modes.AUTO
-
-t = threading.Thread(target=bg, daemon=True )
+        if (not Queue_MD.empty()):
+            if Queue_MD.get == Modes.MANUAL.name:
+                MODE = Modes.MANUAL
+            elif Queue_MD.get == Modes.AUTO.name:
+                MODE = Modes.AUTO
 
 def main():
     t.start()
     while True:
-        print("One run enabled")
         MODES[MODE.value].run()
-    
-def open_serial(sc):
-        try:
-            sc = OpenCatSerialConnection()
-        except:
-            print('Not OpenCat found on', PORT)
-            exit(1)
 
 if __name__=='__main__':
     main()
---
+++ piserver.py
@@ -0,0 +1,27 @@
+import threading
+import zmq
+import queue
+
+context = zmq.Context()
+
+sub_sock_server = context.socket(zmq.SUB)
+sub_sock_server.connect('tcp://*:2273')
+sub_sock_server.setsockopt(zmq.SUBSCRIBE, b'MD:')
+sub_sock_server.setsockopt(zmq.SUBSCRIBE, b'ST:')
+
+
+Queue_MD = queue.Queue()
+Queue_ST = queue.Queue()
+
+def d():
+    while True:
+        r = sub_sock_server.recv().decode('utf-8')
+        if ("MD:" in r):
+            Queue_MD.put(r.replace('MD:', '').strip().upper())
+        elif ("ST:" in r):
+            Queue_ST.put(r.replace('ST:', '').strip().upper())
+        else:
+            return
+
+t = threading.Thread(target=d, daemon=True )
+
