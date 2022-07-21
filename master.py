dc9e07f5f9f2919dd76550cfbc5f3f9cf2a5cc4a
Robin <robinekroth95@hotmail.com>
2022-07-21 12:31:40 +0200

    add so if dog is changed in webapp the dog will return what mode it is in

--- master.py
+++ master.py
@@ -4,7 +4,7 @@ import auto
 import manual
 import zmq
 PORT = "/dev/ttyS0"
-from pisockets import Queue_MD, t
+from pisockets import Queue_MD, Queue_ST, t, pub_sock_alerts, dog
 
 class Modes(Enum):
     MANUAL = 0
@@ -27,6 +27,10 @@ def bg():
             elif mode == Modes.AUTO.name:
                 print("enter auto mode")
                 MODE = Modes.AUTO
+        elif (not Queue_ST.empty()):
+            # We dont really care about the value in the queue, its just to see if a new swap has been made by the webapp, and if it has it returns what mode this dog currently is in
+            Queue_ST.get()
+            pub_sock_alerts.send_string(dog['dog'] + MODE.name)
 
 t1 = threading.Thread(target=bg, daemon=True)
 def main():
--- pisockets.py
+++ pisockets.py
@@ -14,12 +14,14 @@ context = zmq.Context()
 sub_sock_server = context.socket(zmq.SUB)
 sub_sock_server.connect(socket['socket'] + '2273')
 sub_sock_server.setsockopt_string(zmq.SUBSCRIBE, dog['dog'] + ' MD:')
+sub_sock_server.setsockopt_string(zmq.SUBSCRIBE, dog['dog'] + ' ST:')
 
 pub_sock_alerts = context.socket(zmq.PUB)
 pub_sock_alerts.connect(socket['socket'] + '2274')
 
 
 Queue_MD = queue.Queue()
+Queue_ST = queue.Queue()
 
 def d():
     while True:
@@ -29,7 +31,9 @@ def d():
         if ("MD:" in r):
             r = r.replace(dog['dog'], '')
             Queue_MD.put(r.replace('MD:', '').strip().upper())
-            return
+        elif ("ST:" in r):
+            r = r.replace(dog['dog'], '')
+            Queue_ST.put(r.replace('ST:', '').strip().upper())
 
 t = threading.Thread(target=d, daemon=True )
 
